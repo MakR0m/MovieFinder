6c10d09333897ddeb7e5f8053634703d7ce6cef2
Roman <Lamb_54@mail.ru>
2025-05-19 16:02:07 +0700

    Добавлены 2 модели представления сущностей таблиц актеров и фильмов в Data

---
+++ MovieFinder.Data/Entity/Actor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieFinder.Data.Entity
+{
+    public class Actor
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
+        public List<Movie> Movies { get; set; } = new(); //Список ссылок на фильмы (для создания внешних ключей)
+    }
+}
