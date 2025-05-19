27e7aae90a51136a70dc46ac87fa448a8ee9258b
Roman <Lamb_54@mail.ru>
2025-05-19 17:36:12 +0700

    Добавлен интерфейс репозитория для работы с фильмами и его реализация.

---
+++ MovieFinder.Data/Repositories/EfMovieRepository.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieFinder.Data.Data;
+using MovieFinder.Data.Entity;
+using MovieFinder.Logic.Interfaces;
+using MovieFinder.Logic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieFinder.Data.Repositories
+{
+    public class EfMovieRepository /*: IMovieRepository*/
+    {
+        private readonly AppDbContext _context;
+
+        public EfMovieRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<MovieDto>> GetAllWithActorsAsync()
+        {
+            var movies = await _context.Movies.Include(x => x.Actors).ToListAsync(); // Возвращаем список фильмов с актерами
+            return movies.Select(MapToDto).ToList(); // Преобразуем объекты - сущности в объекты бизнес логики. Select перебирает элементы и преобразует каждый объект в новый и возвращает список объектов.
+        }
+
+        private static MovieDto MapToDto(Movie entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+            return new MovieDto
+            {
+                Id = entity.Id,
+                Title = entity.Title,
+                Genre = Enum.TryParse<Genre>(entity.Genre, out var g) ? g : Genre.Unknown,
+                Description = entity.Description,
+                Duration = entity.Duration,
+                ActorList = entity.Actors.Select(MapToDto).ToList(),  //Аналогично преобразуем список сущностей актеров в список сущностей дто. (List<Actor> был IEnumerable то не надо было бы)
+            };
+        }
+
+        private static ActorDto MapToDto(Actor entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+            return new ActorDto
+            {
+                FirstName = entity.FirstName,
+                LastName = entity.LastName,
+            };
+        }
+
+    }
+}
---
+++ MovieFinder.Logic/Interfaces/IMovieRepository.cs
@@ -0,0 +1,14 @@
+﻿using MovieFinder.Logic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieFinder.Logic.Interfaces
+{
+    public interface IMovieRepository // Интерфейс для взаимодействия с репозиторием фильмов проекта Data
+    {
+        Task<IEnumerable<MovieDto>> GetAllWithActorsAsync(); // Получить коллекцию фильмов и связанных с ними актеров
+    }
+}
