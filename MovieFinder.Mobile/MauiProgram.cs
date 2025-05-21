28fb34d3407bbc59809d40685261542e6d24bce8
Roman <Lamb_54@mail.ru>
2025-05-21 14:21:26 +0700

    Регистрация DI в MauiProgram (MovieService, EfMoviePerository, MainPageViewModel)

--- MovieFinder.Mobile/MauiProgram.cs
+++ MovieFinder.Mobile/MauiProgram.cs
@@ -5,6 +5,9 @@ using System;
 using MovieFinder.Logic.Interfaces;
 using MovieFinder.Data.Repositories;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using MovieFinder.Logic.Services;
+using MovieFinder.Mobile.ViewModels;
 
 namespace MovieFinder.Mobile
 {
@@ -20,7 +23,10 @@ namespace MovieFinder.Mobile
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
-            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Filename=movies.db"));
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Filename=movies.db")); //Создает DbContext (главный объект EF core для взаимодействия с бд)
+            builder.Services.TryAddScoped<IMovieRepository,EfMovieRepository>(); //Один экземпляр на время жизни DbContext
+            builder.Services.TryAddScoped<IMovieService, MovieService>(); //Один экземпляр на время жизни DbContext
+            builder.Services.TryAddTransient<MainPageViewModel>(); //Каждый раз при обращении к MainPageViewModel создается новый экземпляр. Привязка в xaml вызывает дефолт конструктор без параметров, а нам нужны зависимости
 
 #if DEBUG
             builder.Logging.AddDebug();
